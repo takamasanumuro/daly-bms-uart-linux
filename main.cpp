0b7bcd9ffac9cf382297afebf0e39fa935e2ac01
adrianowerneck <adrianowerneck@id.uff.br>
2024-11-25 11:29:46 -0300

    Update CMake configuration

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include "daly-bms-uart.h"
 #include <time.h>
+#include <chrono> // Include chrono for timing
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "./libraries/cpp-httplib/httplib.h"
+
+
+int main2(int argc, char **argv) {
+    httplib::SSLClient client("catfact.ninja");
+    client.enable_server_certificate_verification(true);
+    auto result = client.Get("/fact");
+    if (result) {
+        std::cout << result->status << std::endl;
+        std::cout << result->body << std::endl;
+    } else {
+        auto err = result.error();
+        std::cout << "Error: " << err << std::endl;
+    }
+    return 0;
+}
+
+
 int main(int argc, char **argv) {
     Daly_BMS_UART bms(argv[1]);
     bms.Init();
     
     while ( true ) {
+        auto start = std::chrono::high_resolution_clock::now(); // Start timing
+
         bms.update();
+
         std::cout << "\033[0mbasic BMS Data:             \033[32;1m" << bms.get.packVoltage << " V," << bms.get.packCurrent << " A " << bms.get.packSOC << "\% " << std::endl;
         std::cout << "\033[0mPackage Temperature:        \033[32;1m" << bms.get.tempAverage << std::endl;
         std::cout << "\033[0mHighest Cell Voltage Nr:    \033[32;1m" << bms.get.maxCellVNum << " with voltage" << (bms.get.maxCellmV / 1000) << std::endl;
@@ -18,8 +41,12 @@ int main(int argc, char **argv) {
         std::cout << "\033[0mDischarge MOSFet Status:    \033[32;1m" << bms.get.disChargeFetState << std::endl;
         std::cout << "\033[0mCharge MOSFet Status:       \033[32;1m" << bms.get.chargeFetState << std::endl;
         std::cout << "\033[0mRemaining Capacity mAh:     \033[32;1m" << bms.get.resCapacitymAh << std::endl;
-        std::cout << "\033[0m\033[11A\033[0G";
-        sleep(1);
+
+        auto end = std::chrono::high_resolution_clock::now(); // End timing
+        std::chrono::duration<double, std::milli> elapsed = end - start; // Calculate elapsed time
+        std::cout << "\033[0mLoop execution time:        \033[32;1m" << elapsed.count() << " ms" << std::endl;
+
+        std::cout << "\033[0m\033[12A\033[0G";
     }
     return 0;
 }
